8cfa9fa3f97407f77bc84d324ee025d6b6146dec
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-02 17:39:39 -0400

    commit tree 80c0c743a0704cc416f25b2022622b769e90e1a7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+
+    # validate :cat_validator
+    
+    # def cat_validator
+    #     if category == "Fiction" || category == "Non-Fiction"
+    #         return
+    #     else
+    #         return errors.add(:category, "is not included in the list")
+    #     end
+    # end
+
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :check_title
+
+    def check_title
+        if /Won't Believe/i.match?(title) || /Secret/i.match?(title) || /Top \d/i.match?(title) || /Guess/i.match?(title)
+            return
+        else
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
